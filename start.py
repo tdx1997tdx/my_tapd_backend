ba75818c00b3998e293a2fd49c3724e3182f3990
dexuantang <798637048@qq.com>
2022-05-02 15:28:46 +0800

    部署文件增加

--- start.py
+++ start.py
@@ -1,5 +1,4 @@
 import os
-import subprocess
 
 os.system('killall gunicorn')
-subprocess.Popen('nohup /usr/local/python3/bin/gunicorn my_tapd_backend.wsgi -w 4 -b 0.0.0.0:8080 &')
+os.system('/usr/local/python3/bin/gunicorn my_tapd_backend.wsgi -w 4 -b 0.0.0.0:8080')
