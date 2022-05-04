637e5aee49e0195dc8f79e8d268b40e4701afcc3
dexuantang <798637048@qq.com>
2022-05-04 23:16:54 +0800

    修改个人信息功能增加

---
+++ drop_all_table.py
@@ -0,0 +1,23 @@
+import pymysql
+from my_tapd_backend.prd_settings import DATABASES
+
+connection = pymysql.connect(
+    host=DATABASES['default']['HOST'],  # 服务器ip地址
+    port=DATABASES['default']['PORT'],  # mysql默认端口号
+    user=DATABASES['default']['USER'],  # 用户名
+    password=DATABASES['default']['PASSWORD'],  # 密码
+    charset="utf8",  # 字符集
+    db=DATABASES['default']['NAME']  # 数据库
+)
+
+my_cursor = connection.cursor()
+sql = "SELECT concat('DROP TABLE ', table_name, ';') FROM information_schema.tables WHERE table_schema = 'my_tapd_backend';"
+my_cursor.execute(sql)
+data = my_cursor.fetchall()
+for i in data:
+    print(i)
+    my_cursor.execute(i)
+    my_cursor.commit()
+
+my_cursor.close()
+connection.close()
